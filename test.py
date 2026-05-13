551d03de8d40e362930da5eb02bae008612cbe81
Kashchit <thapakashchitbikram@gmail@gmail.com>
2026-05-13 10:44:50 +0000

    Fixed chromedriver path

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
@@ -6,9 +7,11 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+service = Service("/usr/bin/chromedriver")
 
-driver.get("file:///var/lib/jenkins/workspace/devops-pipeline/index.html")
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("file:///var/lib/jenkins/workspace/devops-pipeline-see/index.html")
 
 print("Test Passed")
 
